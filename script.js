b4e01b2b2fb05391f7c73bd1cfbdacae0a31a7a4
Nico235711 <139637937+Nico235711@users.noreply.github.com.>
2024-11-17 21:37:17 -0300

    poniendo comentarios

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-
+// selecciono mis elementos del DOM
 const button = document.querySelector(".cambiar-color")
 const colorHex = document.querySelector(".color-hex")
 
 function generateRandomHex() {
+  // 16 posibilidades para el color hexadecimal
   const digits = "abcdef0123456789"
   let colourHex = "#"
   for (let index = 0; index < 6; index++) {
@@ -16,5 +17,6 @@ function generateRandomHex() {
 button.addEventListener("click", () => {
   const colourHex = generateRandomHex()
   colorHex.textContent = colourHex
+  // cambio el background del body
   document.body.style.backgroundColor = colourHex
 })
